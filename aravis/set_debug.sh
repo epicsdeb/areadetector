f21ffa442f5d49ace4f09721901ba1bdbb54d1a3
Michael Davidsaver <mdavidsaver@bnl.gov>
2011-11-02 14:24:37 -0400

    include aravis GigE 0.1.10

---
+++ aravis/set_debug.sh
@@ -0,0 +1,6 @@
+#export ARV_DEBUG=stream-thread
+#genicam
+export ARV_DEBUG=stream-thread:2,packet:3,gvcp:2
+#export ARV_DEBUG=${ARV_DEBUG}:packet
+#export ARV_DEBUG=${ARV_DEBUG}:evaluator
+#export ARV_DEBUG=${ARV_DEBUG}:stream-thread
